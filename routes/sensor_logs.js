1143f9bdd9d58a7876b6fb191a0c90e74b038163
droza0 <droza0@users.noreply.github.com>
2019-11-10 16:38:11 -0500

    fix

--- routes/sensor_logs.js
+++ routes/sensor_logs.js
@@ -2,10 +2,41 @@ var express = require('express');
 var router = express.Router();
 var SensorData = require('../db/models/sensorData')
 
+
+router.get('/', async(req, res, next) => {
+    try {
+        const allSensorData = await SensorData.findAll();
+        if (allSensorData) {
+            res.json(allSensorData);
+        } else {
+            res.status(404).send("Sensor data not Found")
+        }
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const sensorData = SensorData.findByPk(req.params.id)
+        if (sensorData) {
+            res.json(sensorData);
+        } else {
+            res.status(404).send("Sensor data not Found")
+        }
+    } catch (err) {
+        next(err)
+    }
+})
+
 router.post('/', async (req, res, next) => {
     try {
         const newData = await SensorData.create(req.body);
-        res.json(newData);
+        if (newData) {
+            res.json(newData);
+        } else {
+            res.status(500).send("Sensor data post failed")
+        }
     } catch (err) {
         next(err);
     }
