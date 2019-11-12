5dad4803b635a9f5190a8c97ac2ff4d8aecb2814
Greashun <vncntts@gmail.com>
2019-11-11 20:07:58 -0500

    Enter

--- db/index.js
+++ db/index.js
@@ -6,17 +6,9 @@ const SensorData = require("./models/sensorData.js")
 const User = require("./models/user.js")
 const db = require('./db')
 
-<<<<<<< HEAD
-
-
 Group.hasMany(User)
 User.belongsTo(Group)
 
-=======
-Group.hasMany(User)
-User.belongsTo(Group)
-
->>>>>>> 9a90483a9180b5a8132d9a2dbc121fb37a73fe21
 User.hasMany(SensorData)
 SensorData.belongsTo(User)
 
@@ -28,16 +20,6 @@ LocationHistory.belongsTo(User)
 
 User.belongsToMany(Alert, {through: 'UserToAlert'})
 
-<<<<<<< HEAD
-
-
-
-
-
-
-
-=======
->>>>>>> 9a90483a9180b5a8132d9a2dbc121fb37a73fe21
 module.exports = {
     db,
     User,
