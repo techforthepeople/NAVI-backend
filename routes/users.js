add75b63f74efda3ee9eda5fcc81c8c565c605fc
droza0 <droza0@users.noreply.github.com>
2019-12-16 15:30:40 -0500

    added location history

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { User } = require('../db/index')
 const { ResponderProfile } = require('../db/index')
+const {LocationHistory} = require('../db/index')
 
 
 
